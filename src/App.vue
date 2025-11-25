1711b561f6108f34515c27c9ed8d81c074ed79e5
mickoymouse <30593627+mickoymouse@users.noreply.github.com>
2025-11-25 21:45:55 +0800

    Add static home page.

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,14 @@ import PatternLight from "@/assets/pattern-light.svg";
 </script>
 
 <template>
-  <div class="relative flex flex-col h-full w-full items-center">
+  <div class="relative flex flex-col h-screen w-full items-center">
     <div class="flex justify-center">
       <Navbar />
     </div>
     <PatternLight class="absolute top-0 right-0 translate-x-1/2 -z-5" />
     <PatternLight class="absolute top-0 left-0 translate-y-1/2 -translate-x-1/2 -z-5" />
     <main
-      class="max-w-[630px] h-full w-full pt-12 border-l-2 border-r-2 border-b-2 border-(--neutral-200) overflow-auto"
+      class="h-[calc(100vh-52px)] max-w-[630px] w-full border-l-2 border-r-2 border-b-2 border-(--neutral-200) px-2"
     >
       <RouterView />
     </main>
