69d1a9fc1bd0c72213ea07c64c0cf3efa32efc1f
mickoymouse <30593627+mickoymouse@users.noreply.github.com>
2025-11-25 18:09:12 +0800

    Add styles for the main content

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,15 @@ import PatternLight from "@/assets/pattern-light.svg";
 </script>
 
 <template>
-  <div class="relative">
+  <div class="relative flex flex-col h-full w-full items-center">
     <div class="flex justify-center">
       <Navbar />
     </div>
-    <main>
-      <PatternLight class="absolute top-0 right-0 translate-x-1/2 -z-5" />
-      <PatternLight class="absolute top-0 left-0 translate-y-1/2 -translate-x-1/2 -z-5" />
+    <PatternLight class="absolute top-0 right-0 translate-x-1/2 -z-5" />
+    <PatternLight class="absolute top-0 left-0 translate-y-1/2 -translate-x-1/2 -z-5" />
+    <main
+      class="max-w-[630px] h-full w-full pt-12 border-l-2 border-r-2 border-b-2 border-(--neutral-200) overflow-auto"
+    >
       <RouterView />
     </main>
   </div>
