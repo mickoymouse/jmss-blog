940e3ff9aa5182b0062654a801edffc511e08668
mickoymouse <30593627+mickoymouse@users.noreply.github.com>
2025-11-25 14:05:25 +0800

    Add navbar mockup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Navbar from "@/components/Narbar.vue";
+</script>
 
 <template>
+  <div class="flex justify-center">
+    <Navbar />
+  </div>
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
---
+++ src/components/Narbar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import DarkIcon from "@/icons/icon-moon.svg";
+import LightIcon from "@/icons/icon-sun.svg";
+</script>
+
+<template>
+  <header
+    class="h-[52px] flex items-center justify-between px-2 rounded-md max-w-[640px] min-w-[640px] border"
+  >
+    <img src="@/assets/me.jpg" alt="judge micko's image" class="h-10 w-10 rounded-md" />
+    <nav class="flex gap-4">
+      <ul class="flex gap-6">
+        <RouterLink to="/" tag="li">Home</RouterLink>
+        <RouterLink to="/" tag="li">Blog</RouterLink>
+        <RouterLink to="/" tag="li">About</RouterLink>
+        <RouterLink to="/" tag="li">Newsletter</RouterLink>
+      </ul>
+      <button>
+        <DarkIcon />
+      </button>
+    </nav>
+  </header>
+</template>
