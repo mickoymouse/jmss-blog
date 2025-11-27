e2e24bed6a64eca9f2ae19c821972678ec1446d6
mickoymouse <30593627+mickoymouse@users.noreply.github.com>
2025-11-27 13:52:48 +0800

    Add newsletter ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -82,13 +82,13 @@ const articles = ref([
   <div class="flex flex-col gap-6 pt-12">
     <h1 class="text-2 self-start with-underline">Hi, I'm Paulina 👋</h1>
     <div class="flex flex-col gap-6">
-      <p class="text-7">
+      <p class="text-7 text-(--neutral-600)">
         I'm on a journey to become a front-end web developer. I love building little projects,
         trying out new coding techniques, and sharing what I learn along the way. When I'm not at my
         desk, you'll find me reading, hiking through the mountains, or challenging myself on
         rock-climbing walls.
       </p>
-      <p class="text-7">
+      <p class="text-7 text-(--neutral-600)">
         I started this blog to document my progress, keep myself accountable, and hopefully inspire
         anyone else who's learning to code. Welcome to my corner of the internet, and thanks for
         stopping by!
--- src/views/Newsletter.vue
+++ src/views/Newsletter.vue
@@ -1,1 +1,34 @@
-<template>this is Newsletter</template>
+<template>
+  <div class="flex flex-col pt-12 gap-6">
+    <div class="flex flex-col gap-4">
+      <h1 class="text-2">Newsletter</h1>
+      <p class="text-7 text-(--neutral-600)">
+        Want to stay updated on my latest articles, coding tutorials, and personal adventures? Sign
+        up for my newsletter! It's a simple way to keep track of new posts and occasional coding
+        tips I discover. Just drop your email in the sign-up box, and I'll send you updates whenever
+        there's something new to share.
+      </p>
+      <p class="text-5">
+        I'd love to have you along for the ride and also hear about your own journey!
+      </p>
+    </div>
+    <form action="" class="flex flex-col">
+      <div>
+        <label for="email">Email Address</label>
+        <input
+          id="email"
+          type="email"
+          placeholder="email@example.com"
+          class="w-full border-2 border-(--neutral-200) rounded-[10px] px-4 py-2 bg-(--neutral-0)"
+        />
+      </div>
+      <button
+        type="submit"
+        class="mt-4 bg-(--blue-500) text-(--neutral-900) w-[158px] h-[51px] rounded-[10px] text-6"
+      >
+        Stay Updated
+      </button>
+      <p class="text-8 text-(--neutral-600) mt-2">Unsubscribe anytime. No spam, I promise 🙂</p>
+    </form>
+  </div>
+</template>
