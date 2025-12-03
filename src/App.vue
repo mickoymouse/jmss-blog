68928ae3f79cd877ae14c06e9c8d47fe971cd938
mickoymouse <mckslvstr@gmail.com>
2025-12-03 17:10:07 +0800

    Add mobile navbar

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,7 @@ const { isDarkMode } = useTheme();
 
 <template>
   <div class="relative flex flex-col h-screen w-full items-center">
-    <div class="flex justify-center">
-      <Navbar />
-    </div>
+    <Navbar />
     <PatternLight v-if="!isDarkMode" class="absolute top-0 right-0 translate-x-1/2 -z-5" />
     <PatternDark v-else class="absolute top-0 right-0 translate-x-1/2 -z-5" />
     <PatternLight
@@ -24,7 +22,7 @@ const { isDarkMode } = useTheme();
     />
     <PatternDark v-else class="absolute top-0 left-0 translate-y-1/2 -translate-x-1/2 -z-5" />
     <main
-      class="h-[calc(100vh-52px)] max-w-[630px] w-full border-l-2 border-r-2 border-b-2 border-(--border-color) px-2"
+      class="h-[calc(100vh-52px)] max-w-[calc(100%-32px)] md:max-w-[630px] px-2 w-full border-l-2 border-r-2 border-b-2 border-(--border-color)"
     >
       <Simplebar class="h-[calc(100%-80px)]">
         <RouterView />
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,6 +5,11 @@ import { useRoute } from "vue-router";
 import DarkIcon from "@/icons/icon-moon.svg";
 import LightIcon from "@/icons/icon-sun.svg";
 import { useTheme } from "@/composables/Theme";
+import Menu from "@/icons/icon-menu.svg";
+import MenuClose from "@/icons/icon-menu-close.svg";
+import Divider from "@/components/ui/Divider.vue";
+
+const isMenuOpen = ref(false);
 
 const { isDarkMode, toggleTheme } = useTheme();
 
@@ -73,11 +78,11 @@ onMounted(() => {
 
 <template>
   <header
-    class="h-[52px] flex items-center justify-between p-2 rounded-md max-w-[640px] min-w-[640px] border-2 border-(--border-color) bg-(--nav-bg) mt-5"
+    class="relative h-[52px] flex items-center justify-between p-2 rounded-md max-w-[calc(100%-16px)] md:max-w-[640px] w-full border-2 border-(--border-color) bg-(--nav-bg) mt-5"
   >
     <img src="@/assets/me.jpg" alt="judge micko's image" class="h-10 w-10 rounded-md" />
     <nav class="flex gap-4 items-center">
-      <ul class="flex gap-6 relative">
+      <ul class="md:flex gap-6 relative hidden">
         <div v-for="r in routes" :key="r.name" class="flex flex-col" ref="links">
           <RouterLink class="nav-link" :to="{ name: r.routeName }" tag="li">{{
             r.name
@@ -88,6 +93,39 @@ onMounted(() => {
           class="h-1 bg-(--blue-500) absolute bottom-0 transition-all duration-300"
           aria-hidden="true"
         ></span>
+      </ul>
+      <button
+        class="flex items-center justify-center md:hidden cursor-pointer rounded-lg p-2 focus:outline-none"
+        @click="() => (isMenuOpen = !isMenuOpen)"
+        :class="{
+          'bg-(--neutral-700) text-(--neutral-0) dark:bg-(--neutral-0) dark:text-(--neutral-700)':
+            isMenuOpen,
+        }"
+      >
+        <Menu v-if="!isMenuOpen" />
+        <MenuClose v-else />
+      </button>
+      <ul
+        :class="[
+          'md:hidden absolute left-0 z-9999 -top-50 w-full bg-(--neutral-0) dark:bg-(--neutral-800) border-2 border-(--border-color) mt-4 rounded-[10px] p-4 transition-transform duration-300 ease-in-out',
+          isMenuOpen ? 'translate-y-[calc(100%)]' : '-translate-y-[calc(100%-90px)]',
+        ]"
+      >
+        <div
+          v-for="(r, index) in routes"
+          :key="r.name"
+          class="flex flex-col text-6 gap-4 text-(--neutral-900) dark:text-(--neutral-0)"
+          ref="links"
+        >
+          <RouterLink
+            :class="{ 'mt-4': index != 0 }"
+            class="nav-link"
+            :to="{ name: r.routeName }"
+            tag="li"
+            >{{ r.name }}</RouterLink
+          >
+          <Divider v-if="index < routes.length - 1" />
+        </div>
       </ul>
       <button
         class="border-2 border-(--border-color) bg-(--background) p-2 rounded-md cursor-pointer"
